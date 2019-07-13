a8b144f1abe3229f6c74a678bb939bfa6d6756eb
Jordan Banasik <woovie@woovie.net>
2019-07-13 01:26:09 -0500

    Test for a class based system

--- pokescrape.py
+++ pokescrape.py
@@ -1,4 +1,20 @@
 import aiohttp, json
 
 class scraper():
+    async def __init__(self, url):
+        self.url = url
+    async def load(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"{self.url}") as r:
+                if r.status == 200:
+                    returned = await r.content
+                    self.returned = returned
+                    return True
+                else:
+                    return False
 
+scrape = scraper("http://woovie.net/")
+scrapeLoad = scrape.load()
+print(scrapeLoad)
+if scrapeLoad:
+    print(scrape.returned)
